d5b96aa9e14e916fe224c9f12b24f4e27c4ee537
kevingg <kawaigin@gmail.com>
2020-06-04 20:40:33 -0700

    Made the project use typescript

---
+++ dango-react/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Card } from '@rmwc/card';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Card>
+      Hello World.
+    </Card>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
