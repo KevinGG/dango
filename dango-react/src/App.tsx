3e95b7baf012979b4f1758079bcf55c49aeb82da
kevingg <kawaigin@gmail.com>
2020-06-06 12:38:30 -0700

    Added style directory with default css into the project.

--- dango-react/src/App.tsx
+++ dango-react/src/App.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { Card } from '@rmwc/card';
 
 import '@rmwc/card/styles';
+import './style/App.css';
 
 export class App extends React.Component {
   render() {
     return (
-     <React.StrictMode>
-      <Card>
-        Hello World.
-      </Card>
+      <React.StrictMode>
+        <div className="CardContainer">
+          <Card>
+            Hello World.
+          </Card>
+        </div>
       </React.StrictMode>
     );
   }
